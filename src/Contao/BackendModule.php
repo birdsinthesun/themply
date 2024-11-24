d0e716a9efedd63d80813f4d2713e0eac2239dd4
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2024-11-24 18:02:24 +0100

    debug

---
+++ src/Contao/BackendModule.php
@@ -0,0 +1,10 @@
+// src/Contao/BackendModule.php
+namespace Bits\Themply\Contao;
+
+class BackendModule
+{
+    public function generate(): string
+    {
+        return 'string content';
+    }
+}
--- src/Resources/config/config.php
+++ src/Resources/config/config.php
@@ -7,10 +7,11 @@ return static function (ContainerConfigurator $containerConfigurator) {
 $services = $containerConfigurator->services();
 
 // Registriere das Backend-Modul
-$GLOBALS['BE_MOD']['tl_modules'] = [
+$GLOBALS['BE_MOD']['design'] = [
 'themply' => [
 'tables' => ['tl_themply'],
-'icon' => 'bundles/themply/icon.svg',
+'icon' => 'birdsinthesun/themply/icon.png',
+ 'callback' => \Bits\Themply\Contao\BackendModule::class
 ],
 ];
 };
