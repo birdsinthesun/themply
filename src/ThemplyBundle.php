31ff91e9db0762679685f040741cbb5be119f44e
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2024-11-24 22:13:19 +0100

    Update ThemplyBundle.php

--- src/ThemplyBundle.php
+++ src/ThemplyBundle.php
@@ -2,17 +2,12 @@
 
 namespace Bits\Themply;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class ThemplyBundle extends Bundle,AbstractBundle
+class ThemplyBundle extends AbstractBundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
     
     public function loadExtension(
         array $config, 
