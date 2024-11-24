65868d5029f54b992a3c74fc30f40db00e1cf4f1
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2024-11-24 22:51:41 +0100

    Update LuckyController.php

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,7 +4,7 @@ namespace Bits\Themply\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as AbstractController;
 
 class LuckyController extends AbstractController
 {
